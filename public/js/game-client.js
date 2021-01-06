6619c5571ac19619f4bbeb20d7034aaac02f35c4
Ryan Chan <52082100+rac99@users.noreply.github.com>
2021-01-05 19:23:38 -0500

    attempt at word-check integration

--- public/js/game-client.js
+++ public/js/game-client.js
@@ -9,6 +9,36 @@ $(document).ready(function () {
     $("#main-word").text('[' + word + ']');
   }
 
+  // check word function
+  function checkWord(e) {
+    e.preventDefault();
+    
+    axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`, {
+        })
+        .then((res) => {
+            
+            if (res.status !== 200) {
+                
+                if (res.status === 404) {
+                    console.log("This is not a word")
+                }
+            } else {
+                if (word !== res.word){
+                    console.log("This is a misspelled word")
+                } else {
+                  console.log("This is a word")
+                }
+            }
+            
+        })
+        .catch((error) => {
+            console.log(error.message);
+            //console.error(error)
+        })
+
+
+  }
+
   socket.on("load word", (str) => {
     word = str;
     renderWord();
@@ -49,4 +79,12 @@ $(document).ready(function () {
     socket.emit("start game", code);
   });
 
+  // check word
+  $("#check-word").on("click", (e) => {
+    e.preventDefault();
+    checkWord();
+    //socket.emit("check word", word, code);
+  });
+
 });
+
