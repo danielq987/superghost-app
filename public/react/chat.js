f2dce0d6e7c9e3609f9d4923437a12c182afadc2
bonnielu <bonnielu2002@gmail.com>
2021-01-07 19:28:17 -0500

    chat functionality finished

--- public/react/chat.js
+++ public/react/chat.js
@@ -91,24 +91,6 @@ const Chat = () => {
                                     )
                                 }
                             })()}
-                            {/* {
-                            isSentByCurrentUser
-                            ? (
-                                <div className="messageContainer justifyEnd">
-                                    <p className="sentText pr-10">{userName}</p>
-                                    <div className="messageBox backgroundBlue">
-                                        <p className="messageText colorWhite">{message['text']}</p>
-                                    </div>
-                                </div>
-                            )
-                            : (
-                                <div className="messageContainer justifyStart">
-                                    <div className="messageBox backgroundLight">
-                                        <p className="messageText colorDark">{message['text']}</p>
-                                    </div>
-                                    <p className="sentText pl-10">{message['user']}</p>
-                                </div>
-                            )} */}
                         </div>)}
                 </div>
                 
