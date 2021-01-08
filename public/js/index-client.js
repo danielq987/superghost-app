09ceda58ddfe5f5ac3993377a99b14636fc164bc
danielq987 <danielq987@gmail.com>
2021-01-08 16:45:58 -0500

    Implemented python child process within API.

--- public/js/index-client.js
+++ public/js/index-client.js
@@ -6,26 +6,30 @@ $(document).ready(function () {
     $("#create-room-btn").after("<p>Loading...</p>");
     $("#create-room-btn").toggle();
     let name = $("#game-name").val();
-    axios.post('/api/create-game', {
-      name: name
-    }).then(response => {
-      console.log("code: " + response.data.code);
-      window.location.replace(`/game/${response.data.code}`)
-    }).catch(error => {
-      console.error("There was an error making the request." + error);
-    })
+    axios
+      .post("/api/create-game", {
+        name: name,
+      })
+      .then((response) => {
+        console.log("code: " + response.data.code);
+        window.location.replace(`/game/${response.data.code}`);
+      })
+      .catch((error) => {
+        console.error("There was an error making the request." + error);
+      });
   });
 
-  $("#join-room-btn").on("click", async function() {
+  $("#join-room-btn").on("click", async function () {
     let name = $("#game-name").val();
     let code = $("#game-code").val().toUpperCase();
 
     try {
       let gameInfo = await axios.get(`/api/games/${code}`);
+      console.log(gameInfo)
       if (gameInfo.data != null && gameInfo.data.state == 0) {
         // add the new player's SID to the database
-      let response = axios.put(`/api/join-game/${code}`, {name: name});
-      window.location.replace(`/game/${code}`);
+        let response = axios.put(`/api/join-game/${code}`, { name: name });
+        window.location.replace(`/game/${code}`);
       } else {
         console.log("room doesnt exist, or not authorized");
       }
@@ -34,23 +38,28 @@ $(document).ready(function () {
     }
 
     // get the game info to ensure that the room exists and is open
-    axios.get(`/api/games/${code}`).then(response => {
-      console.log(response.data);
-      if (response.data != null && response.data.state == 0) {
-        // add the new player's SID to the database
-        axios.put(`/api/join-game/${code}`, {
-          name: name
-        }).then(putResponse => {
-          console.log(putResponse);
-          // redirect to game lobby
-          window.location.replace(`/game/${response.data.code}`);
-        }).catch(error => {
-          console.error("There was an error making the request." + error);
-        });
-      }
-    }).catch(error => {
-      console.error("Could not get the game ID" + error);
-    });
-  })
-
+    axios
+      .get(`/api/games/${code}`)
+      .then((response) => {
+        console.log(response.data);
+        if (response.data != null && response.data.state == 0) {
+          // add the new player's SID to the database
+          axios
+            .put(`/api/join-game/${code}`, {
+              name: name,
+            })
+            .then((putResponse) => {
+              console.log(putResponse);
+              // redirect to game lobby
+              window.location.replace(`/game/${response.data.code}`);
+            })
+            .catch((error) => {
+              console.error("There was an error making the request." + error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error("Could not get the game ID" + error);
+      });
+  });
 });
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,12 @@ const cookies = require("../helpers/cookies");
 
 router.use(cookies.assignSID);
 
-router.get('/', function (req, res, next) {
-  res.render('index', {title: 'Express is pretty pog!!!'});
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express is pretty pog!!!" });
+});
+
+router.get("/ai", function (req, res, next) {
+  res.render("game", { ai: true });
 });
 
 module.exports = router;
-
